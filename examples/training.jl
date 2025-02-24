7ee7d606ed8ee94c91b7054859ee8559d3db0342
Alexander Barth <barth.alexander@gmail.com>
2025-02-24 18:14:18 +0100

    increase checkpoint_epoch

--- examples/training.jl
+++ examples/training.jl
@@ -54,7 +54,7 @@ isvalid = nothing
 
 
 batch_size = 64
-checkpoint_epoch = 20
+checkpoint_epoch = 200
 nb_epochs = 140
 nb_epochs =  20
 #nb_epochs =  100
@@ -222,7 +222,7 @@ dl = Flux.DataLoader(dd; batchsize = batch_size, shuffle=true,
 
 alpha, alpha_bar, sigma, losses = @time train!(
     model,dl;
-    device = gpu,
+    device = device,
     nb_epochs = nb_epochs,
     learning_rate = learning_rate,
     batch_size = batch_size,
