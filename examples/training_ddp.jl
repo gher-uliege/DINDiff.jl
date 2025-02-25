f85c3c2d7450577da22be996135acbad418282ac
Alexander Barth <barth.alexander@gmail.com>
2025-02-25 10:12:22 +0100

    set JULIA_PROJECT

--- examples/training_ddp.jl
+++ examples/training_ddp.jl
@@ -1,7 +1,16 @@
 using MPI
 
 ENV["PARALLEL"] = "true"
+ENV["JULIA_PROJECT"] = dirname(@__FILE__)
 
-run(`$(mpiexec()) -n 2 julia training.jl`)
+@show ENV["JULIA_PROJECT"]
+np = 2
+#np = 1
+
+run(`$(mpiexec()) -n $np julia training.jl`)
 
 #run(`$(mpiexec()) -n 2 printenv`)
+
+# mirlo
+# 198.215347 seconds 1 GPU: loss 0.022
+# 207 s 2 GPUs: 0.024
